fa04303d76607c18e1c864b7e5329b6e0f71640d
Amier Ali <amier_1998@hotmail.com>
2025-01-27 20:44:07 +0000

    set up clerk for the project

--- data/env/client.ts
+++ data/env/client.ts
@@ -1,7 +1,14 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
-    emptyStringAsUndefined: true,
-    client: {},
-    experimental__runtimeEnv: {},
+  emptyStringAsUndefined: true,
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  },
+
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
 });
