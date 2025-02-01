423dd74c113633f6afca1cbab379dbc80022401a
Amier Ali <amier_1998@hotmail.com>
2025-02-01 17:54:44 +0000

    start UI on homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,40 @@
 import { Toaster } from "@/components/ui/sonner";
 import {
-    ClerkProvider,
-    SignInButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
+  ClerkProvider,
+  SignInButton,
+  SignUp,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <ClerkProvider>
-            <html lang="en">
-                <body>
-                    <nav className="p-2">
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
-                    </nav>
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <div className="flex items-center justify-center mt-20 text-7xl">
+            Get-Fit-Ai
+          </div>
+          <nav className="p-4 flex space-x-4 items-center justify-center mt-7">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </nav>
 
-                    {children}
-                    <Toaster />
-                </body>
-            </html>
-        </ClerkProvider>
-    );
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
